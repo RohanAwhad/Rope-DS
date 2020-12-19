ac18195bc15ca38c2421444e0409e6818e0f4539
Rohan Awhad <rohanavad007@gmail.com>
2020-12-19 22:21:59 +0530

    working

--- src/rope.py
+++ src/rope.py
@@ -56,34 +56,3 @@ class Concatenation(Rope):
 
     def __str__(self):
         return str(self.left) + str(self.right)
-
-
-# Testing framework
-
-
-def equals(rope, expected):
-    actual = str(rope)
-    if actual == expected:
-        return
-    print(actual, "didn't equal", expected)
-    raise Exception
-
-
-equals(to_rope("abc"), "abc")
-equals(to_rope("abcde").substring(1, 4), "bcd")
-equals(to_rope("abcde").substring(1, 4).substring(1, 2), "c")
-equals(to_rope("abc").concatenate("de"), "abcde")
-equals(to_rope("abcde").delete(1, 4), "ae")
-
-assert len(to_rope("abcde")) == 5
-assert len(to_rope("abcde").substring(1, 4)) == 3
-
-equals(to_rope("abe").insert(to_rope("cd"), 2), "abcde")
-
-equals(to_rope("abc") + "de", "abcde")
-equals(to_rope("abc") + to_rope("de"), "abcde")
-
-equals(to_rope("abcde")[1:4], "bcd")
-equals((to_rope("abc") + to_rope("de"))[1:4], "bcd")
-equals(to_rope("abcde")[2], "c")
-equals((to_rope("abc") + to_rope("de"))[2], "c")
