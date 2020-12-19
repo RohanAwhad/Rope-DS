2637321f3be5b75bf7f2b2a4aa164b7a1420443d
Rohan Awhad <rohanavad007@gmail.com>
2020-12-19 18:33:13 +0530

    working

--- rope.py
+++ rope.py
@@ -11,4 +11,12 @@ def to_rope(string):
     return string
 
 
+class Rope:
+    def __init__(self, string):
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+
 assert str(to_rope("abc")) == "abc"
